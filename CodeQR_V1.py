8e949a0efae09318792a7204dc64327af45549ac
Jean-Sebastien Proulx <jsproulx4@gmail.com>
2023-11-02 11:16:59 -0400

    Detection du code QR

--- CodeQR_V1.py
+++ CodeQR_V1.py
@@ -1,9 +1,12 @@
+"""
+Code qui genere un code QR avec l'information qu'on veut d'encoder dedans.
+"""
 import qrcode
 import numpy as np
 # data to encode
-data = "The Necrons are a "
+data = "ceci est un test"
 # instantiate QRCode object
-qr = qrcode.QRCode(version=1, box_size=10, border=4)
+qr = qrcode.QRCode(version=1, box_size=10, border=1)
 # add data to the QR code
 qr.add_data(data)
 # compile the data into a QR code array
@@ -11,6 +14,6 @@ qr.make()
 # print the image shape
 print("The shape of the QR image:", np.array(qr.get_matrix()).shape)
 # transfer the array into an actual image
-img = qr.make_image(fill_color="white", back_color="black")
+img = qr.make_image(fill_color="black", back_color="white")
 # save it to a file
 img.save("necrons.png")
--- LectureQR_V1.py
+++ LectureQR_V1.py
@@ -1,16 +1,33 @@
 import cv2
+from pyzbar.pyzbar import decode
+import numpy as np
+
+
 # read the QRCODE image
-img = cv2.imread("necrons.png")
+img = cv2.imread('necrons.png')
 
-# initialize the cv2 QRCode detector
-detector = cv2.QRCodeDetector()
 
-# initialize the cv2 QRCode detector
-detector = cv2.QRCodeDetector()
 
-# The detectAndDecode() function takes an image as an input and decodes it 
-# to return a tuple of 3 values: the data decoded from the QR code, the output 
-# array of vertices of the found QR code quadrangle, and the output image containing 
-# rectified and binarized QR code.
-data, bbox, straight_qrcode = detector.detectAndDecode(img)
+
+#configuration de la capture video
+capture = cv2.VideoCapture(1)
+capture.set(3,640)      #resolution de l'image
+capture.set(4,480)
+
+while True:
+
+    success,img = capture.read()    #succes = indicateure de reussite, img = image capture par la cam
+    for barcode in decode(img):
+        monData = barcode.data.decode('utf-8')
+        print (monData)
+
+        #pour faire le polygone autour du code qr
+        points = np.array([barcode.polygon], np.int32)
+        points = points.reshape((-1,1,2))
+        cv2.polylines(img,[points],True,(255,0,255),5)
+        points2 = barcode.rect
+        cv2.putText(img, monData,(points2[0],points2[1]),cv2.FONT_HERSHEY_SIMPLEX,0.9,(255,0,255),2)
+
+    cv2.imshow('Result',img)
+    cv2.waitKey(1)
 
