6302d63a0580eee70c1945cd54ad159b2c4cfcc6
MOUSTACHEMAN167 <123669816+MOUSTACHEMAN167@users.noreply.github.com>
2024-05-15 13:53:20 -0500

    Update main.py

--- InteractoBot/Logiciel/Pi_bras_robot/main.py
+++ InteractoBot/Logiciel/Pi_bras_robot/main.py
@@ -7,14 +7,30 @@ import time
 import serial
 from multiprocessing import Process, Value, Lock
 import random
+#import pyttsx3
 
 
 
 #test sur le plan x,y, l'angle du tool est de 0,0,0
 mc = MyCobot('/dev/ttyAMA0',1000000)
 mc.set_gripper_ini()
+
+
+
+
+
+
 #Configuration du serial port
 serial_Port = serial.Serial('/dev/ttyUSB0', 9600)
+
+
+
+
+
+
+#pour le tts
+#engine = pyttsx3.init()
+#engine.setProperty('voice', 'french')
 
 
 
@@ -27,10 +43,10 @@ serial_Port = serial.Serial('/dev/ttyUSB0', 9600)
 #Read_flag = variable de verif de lecture sur le port serie (1 ou 0)
 #Grabber_state = Valeur envoye par le jetson pour l'etat de la pince (1 ou 0)
 #Lock = mutex()
+
 def read_serial(recep_Z,recep_Y,read_flag,lock,Grabber_state,Grabber_twist):
     while True:
         received_string = serial_Port.readline().decode()           #recoi la string par le port serie
-        #print(received_string)
         if received_string:
             temps = received_string.split(',')
             if len(temps) == 4:
@@ -47,10 +63,8 @@ def read_serial(recep_Z,recep_Y,read_flag,lock,Grabber_state,Grabber_twist):
                 lock.release()
                 lock.acquire()
                 Grabber_twist.value = int(temps[3])
-                print(Grabber_twist.value)
                 lock.release()
                 time.sleep(0.001)
-                #print(recep_Y.value,recep_Z.value)
             lock.acquire()
             read_flag.value = 1
             lock.release()
@@ -96,7 +110,7 @@ def idle_act1():
     r=255
     g=0
     b=255
-    #TODO : mettre la vitesse de deplacvement plus vite et faire la boucle des lumiere independante
+
     while flag == False:
         if i < 90 and sense==1:
             mc.send_angles([90,0,i,0,90,45],80)
@@ -150,6 +164,9 @@ def idle_act1():
                 flag = True
 
 def dodo_attente():
+
+    mc.set_gripper_state(1,100)
+
     #Variables pour la position de relachement des servos du robot
     pause_x = -14.3
     pause_y = -20.2
@@ -166,6 +183,9 @@ def dodo_attente():
 
 def mouvement(recep_Z,recep_Y,shared_Read_flag,lock,Grabber_state,Grabber_twist):
 
+    #Variable qui compte le nombre d'iteration d'activite d'attente
+    iteration_att = 0
+
     #Compteur pour compter le temps dinactivite du robot
     compteur_inactivite = 0
 
@@ -173,7 +193,7 @@ def mouvement(recep_Z,recep_Y,shared_Read_flag,lock,Grabber_state,Grabber_twist)
     vitesse = 90
             
     #Constante pour les types de mouvements
-    Angulaire = 0
+    type_mouvements = 0
     #lineaire = 1
     #Angulaire = 0
 
@@ -184,7 +204,7 @@ def mouvement(recep_Z,recep_Y,shared_Read_flag,lock,Grabber_state,Grabber_twist)
             Distance_preset_X = 140   
 
             #Valeure par defaut de la rotation de la pince
-            ry = 0
+            ry = -45
             
             #Valeure par defaut des angle de la "tete" du robot
             rx_m = -81
@@ -215,15 +235,17 @@ def mouvement(recep_Z,recep_Y,shared_Read_flag,lock,Grabber_state,Grabber_twist)
             dep_Z = mmpix_Z*temp_recep_Z
             dep_Z = dep_Z + 185          #le +185 a pour but de faire respecter le limites de hauteur aux mouvement de haut en bas
             #print(dep_Z)
+
             #Travail de la variable de rotation de la pince afin qu'elle respecte certaines limites
             if Grabber_twist.value > 5 or Grabber_twist.value < -5:
-                ry = Grabber_twist.value
-                if ry >= 90:
-                    ry = 90
-                elif ry <= -90:
-                    ry = -90
+                ry = (Grabber_twist.value-45)
+                if ry >= 135:
+                    ry = 135
+                elif ry <= -45:
+                    ry = -45
             else:
-                ry = 0
+                ry = -45
+            
 
             #Si les valeures de mouvement sont dans ces limites, ne fait rien sinon bouge
             if dep_Y > -115 and dep_Y < 115 and dep_Z < 210 and dep_Z >= 185 or temp_recep_Y ==0 and temp_recep_Z == 0 or dep_Z <= 200:
@@ -234,38 +256,33 @@ def mouvement(recep_Z,recep_Y,shared_Read_flag,lock,Grabber_state,Grabber_twist)
                 if Grabber_state.value == 1:
                     mc.set_gripper_state(1,100)
                     if dep_Y >= 0:
-                        mc.send_coords([Distance_preset_X,dep_Y,dep_Z,rx_p,ry,rz_p],vitesse,Angulaire)
+                        mc.send_coords([Distance_preset_X,dep_Y,dep_Z,rx_p,ry,rz_p],vitesse,type_mouvements)
                     elif dep_Y < 0:
-                        mc.send_coords([Distance_preset_X,dep_Y,dep_Z,rx_m,ry,rz_m],vitesse,Angulaire)
+                        mc.send_coords([Distance_preset_X,dep_Y,dep_Z,rx_m,ry,rz_m],vitesse,type_mouvements)
                     time.sleep(0.001) 
                 else:
                     #Si le microcontroleur envoi que la pince doit etre ouverte, ouvre la pince avec une vitesse de 100% 
                     mc.set_gripper_state(0,100)
                     if dep_Y >= 0:
-                        mc.send_coords([Distance_preset_X,dep_Y,dep_Z,rx_p,ry,rz_p],vitesse,Angulaire)
+                        mc.send_coords([Distance_preset_X,dep_Y,dep_Z,rx_p,ry,rz_p],vitesse,type_mouvements)
                     elif dep_Y < 0:
-                        mc.send_coords([Distance_preset_X,dep_Y,dep_Z,rx_m,ry,rz_m],vitesse,Angulaire)
-                    time.sleep(0.001) 
-            time.sleep(0.001)
+                        mc.send_coords([Distance_preset_X,dep_Y,dep_Z,rx_m,ry,rz_m],vitesse,type_mouvements)
+                    time.sleep(0.01) 
+            time.sleep(0.01)
             shared_Read_flag.value = 0
         
         #compteur pour tomber en veille apres un certain temps
         elif compteur_inactivite < 200: 
-            if compteur_inactivite == 0 or compteur_inactivite == 1:
-                mc.set_gripper_state(1,100)
             compteur_inactivite = compteur_inactivite + 1
-            #print(compteur_inactivite)  #Lecture pour tester laugementation de la valeure de compteur dinactivite
 
         elif compteur_inactivite == 200 :
             dodo_attente()
             compteur_inactivite = 300
 
-        elif compteur_inactivite >= 300 and compteur_inactivite < 500: #Normalement 6300
+        elif compteur_inactivite >= 300 and compteur_inactivite < 3300: #Normalement 3300
             compteur_inactivite = compteur_inactivite + 1
-            #print(compteur_inactivite)
 
-        elif compteur_inactivite == 500:   #Normalement 6300
-            #print("test")
+        elif compteur_inactivite == 3300 and iteration_att < 24:   #Normalement 3300
             selection_activite = random.randint(0,1)
             if selection_activite == 0:
                 idle_act0()
@@ -275,8 +292,10 @@ def mouvement(recep_Z,recep_Y,shared_Read_flag,lock,Grabber_state,Grabber_twist)
                 dodo_attente()
 
             compteur_inactivite = 300
+            iteration_att = iteration_att + 1
 
         time.sleep(0.05) 
+
 
 def main():
 
@@ -289,13 +308,16 @@ def main():
     Grabber_state = Value('i',0)
     Grabber_twist = Value('i',0)
     
-    #definition des process
+    #definition des process et thread
     process_Serial = Process(target=read_serial, args=(shared_Recep_Z,shared_Recep_Y,shared_Read_flag,lock,Grabber_state,Grabber_twist))
     process_Mouvement = Process(target=mouvement, args=(shared_Recep_Z,shared_Recep_Y,shared_Read_flag,lock,Grabber_state,Grabber_twist))
+
+
 
     #demarrage des process
     process_Serial.start()
     process_Mouvement.start()
+
 
 
 #Appel de la fonction main au démarrage du programme
